8895fbdf6116c99ee058be6596609605a8492b00
MarkuwM <silvaalderlidson@gmail.com>
2024-11-07 22:09:02 -0300

    aula programação 2 07/11/24

--- matrizzz/src/JogoDaVelha.java
+++ matrizzz/src/JogoDaVelha.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class JogoDaVelha {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println("Jogo da Velha");
+        System.out.print("Nome do Jogador 1:");
+        String jogador1 = input.nextLine();
+        System.out.print("Nome do Jogador 2:");
+        String jogador2 = input.nextLine();
+        System.out.println("Nova Partida: " + jogador1 + " vs " + jogador2);
+
+        for (int i = 0; i < velha.length; ++i) {
+            for (int j = 0; j < velha.length; ++j) {
+                System.out.print(velha[i][j] + "\t");
+            }
+            System.out.println();
+        }
 
     }
 }
---
+++ matrizzz/src/VelhaModulo.java
@@ -0,0 +1,21 @@
+public class VelhaModulo {
+    public static char receberCaractere (char a, char b) {
+
+        return 0;
+    }
+
+    public static char rodada (char a, char b) {
+        char[][] velha1 = new char[3][3];
+        char[][] velha2 = new char[3][3];
+
+        do {
+            for (int i = 0; i < 3; ++i) {
+                for (int j = 0; j < 3; ++j) {
+
+                }
+            }
+            while ()
+        }
+        return 0;
+    }
+}
