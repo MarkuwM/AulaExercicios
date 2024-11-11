681b4a368742b8daa3cfec3ffbe9dd376b636558
MarkuwM <silvaalderlindson@gmail.com>
2024-11-11 20:47:58 -0300

    matriz

--- matrizzz/src/JogoDaVelha.java
+++ matrizzz/src/JogoDaVelha.java
@@ -13,4 +13,21 @@ public class JogoDaVelha {
         System.out.println("Nova Partida: " + jogador1 + " vs " + jogador2);
 
     }
+    public static char receberCaractere (char a, char b) {
+
+        return 0;
+    }
+    public static char rodada (char a, char b) {
+        char[][] velha1 = new char[3][3];
+        char[][] velha2 = new char[3][3];
+
+        do {
+            for (int i = 0; i < 3; ++i) {
+                for (int j = 0; j < 3; ++j) {
+                    System.out.print(velha1[i][j]);
+                }
+            }
+        } while ();
+        return 0;
+    }
 }
