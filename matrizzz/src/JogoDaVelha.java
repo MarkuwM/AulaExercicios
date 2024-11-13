ee4b90105d3cf93a3038d146217fa3ed0d750f32
MarkuwM <silvaalderlidson@gmail.com>
2024-11-13 19:05:20 -0300

    13/11/24

--- matrizzz/src/JogoDaVelha.java
+++ matrizzz/src/JogoDaVelha.java
@@ -1,33 +1,68 @@
+package ads.jogo.da.velha;
+
 import java.util.Scanner;
 
 public class JogoDaVelha {
+
     public static void main(String[] args) {
+        char[][] tabuleiro = new char[3][3];
         Scanner input = new Scanner(System.in);
 
+        System.out.println("Digite o nome do primeiro jogador");
+        String jogadorA = input.nextLine();
 
-        System.out.println("Jogo da Velha");
-        System.out.print("Nome do Jogador 1:");
-        String jogador1 = input.nextLine();
-        System.out.print("Nome do Jogador 2:");
-        String jogador2 = input.nextLine();
-        System.out.println("Nova Partida: " + jogador1 + " vs " + jogador2);
+        System.out.println("Digite o nome do segundo jogador");
+        String jogadorB = input.nextLine();
 
-    }
-    public static char receberCaractere (char a, char b) {
+        inicializarTabuleiro(tabuleiro);
+        boolean isJogoAcabou = false;
+        int linha, coluna = 0;
+        String jogadorAtual = jogadorA;
 
-        return 0;
-    }
-    public static char rodada (char a, char b) {
-        char[][] velha1 = new char[3][3];
-        char[][] velha2 = new char[3][3];
+        while (!isJogoAcabou) {
 
-        do {
-            for (int i = 0; i < 3; ++i) {
-                for (int j = 0; j < 3; ++j) {
-                    System.out.print(velha1[i][j]);
-                }
+            System.out.println(jogadorAtual + ", digite a linha da sua jogada");
+            linha = input.nextInt();
+
+            System.out.println(jogadorAtual + ", digite a coluna da sua jogada");
+            coluna = input.nextInt();
+
+            realizarJogada(linha, coluna);
+            isJogoAcabou = verificarFimPartida();
+            exibirTabuleiro();
+            if (jogadorAtual == jogadorA) {
+                jogadorAtual = jogadorB;
             }
-        } while ();
-        return 0;
+            else {
+                jogadorAtual = jogadorA;
+            }
+        }
+
+    }
+
+    public static void inicializarTabuleiro(char[][] tabuleiro) {}
+
+    public static void exibirTabuleiro(){
+        System.out.println("Exibindo o tabuleiro");
+    }
+
+    public static void realizarJogada(int linha, int coluna) {
+        System.out.println("Realizando a jogada");
+        if (isJogadaValida(linha, coluna)) {
+            //atualizar matriz com a jogada
+        }
+    }
+
+    public static boolean isJogadaValida(int linha, int coluna) {
+        System.out.println("verificando se a jogada é válida");
+        //verificar se a matriz ta vazia na posição da jogada
+        return false;
+    }
+
+    public static boolean verificarFimPartida() {
+        System.out.println("verificando se a partida acabou");
+        //validar se alguém venceu ou se a partida empatou
+        //ganha quando a matriz tiver o mesmo valor em linha, coluna ou diagonal
+        return false;
     }
 }
---
+++ matrizzz/src/JogoDaVelhaX.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class JogoDaVelhaX {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println("Jogo da Velha");
+        System.out.print("Nome do Jogador 1:");
+        String jogador1 = input.nextLine();
+        System.out.print("Nome do Jogador 2:");
+        String jogador2 = input.nextLine();
+        System.out.println("Nova Partida: " + jogador1 + " vs " + jogador2);
+
+    }
+    public static char receberCaractere (char a, char b) {
+
+        return 0;
+    }
+    public static char rodada (char a, char b) {
+        char[][] velha1 = new char[3][3];
+        char[][] velha2 = new char[3][3];
+
+        return 0;
+    }
+}
