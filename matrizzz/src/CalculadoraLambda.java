e1026f5f520a9c39af830a63b11ed8319f966350
MarkuwM <silvaalderlindson@gmail.com>
2025-01-22 21:44:52 -0300

    lambda

---
+++ matrizzz/src/CalculadoraLambda.java
@@ -0,0 +1,5 @@
+public class CalculadoraLambda {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ matrizzz/src/ListaLambda.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ListaLambda {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> pares = numeros.stream()
+                .filter(n -> n % 2 == 0)
+                .collect(Collectors.toList());
+        System.out.println(pares);  // Saída: [2, 4, 6]
+
+    }
+}
