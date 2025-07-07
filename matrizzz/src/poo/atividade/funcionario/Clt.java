c5ac03e3c83622324be2779ede3e19e7c8d90b31
MarkuwM <silvaalderlindson@gmail.com>
2025-07-07 17:19:24 -0300

    commit

---
+++ matrizzz/src/poo/atividade/funcionario/Clt.java
@@ -0,0 +1,14 @@
+package poo.atividade.funcionario;
+
+public class Clt extends Funcionario {
+    private double salariobase;
+    private double bonus;
+    
+    public Clt(String nome, String cpf, double salario) {
+        super(nome, cpf, salario);
+    }
+
+    public void calcularSalario() {
+        
+    }
+}
